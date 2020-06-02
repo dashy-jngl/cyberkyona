e7dbac8d256a10c49a93f3f78ae088efd9d3e83f
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2020-06-02 11:57:11 +0930

    Add files via upload

--- animal/animal.py
+++ animal/animal.py
@@ -35,7 +35,7 @@ class Animal(BaseCog):
     async def cat(self, ctx):
         """Shows a cat"""
         try:
-            fp = random.choice(os.listdir("http://dash.pallas.feralhosting.com/.joshi/"))
+            fp = random.choice(os.listdir("http://dash.pallas.feralhosting.com/.joshi"))
             await bot.send_file(ctx.message.channel, "randomimagefoldername/{}".format(fp))
         except:
             await ctx.send("API Error")
