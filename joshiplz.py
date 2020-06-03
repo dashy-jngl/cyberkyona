074192b577178df645a41ee9e4148082e4759c1f
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2020-06-03 21:34:00 +0930

    Add files via upload

---
+++ joshiplz.py
@@ -0,0 +1,49 @@
+# Post animal pics by Eslyium#1949 & Yukirin#0048
+
+# Discord
+import discord
+
+# Red
+from redbot.core import commands
+
+
+# Libs
+import os
+import random
+#import urllib.request
+
+
+import aiohttp
+#import asyncio
+#import io
+#import logging
+#from typing import Awaitable, Callable
+
+BaseCog = getattr(commands, "Cog", object)
+
+
+class Joshiplz(BaseCog):
+    #JoshiSpam!
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+     
+    @commands.command()
+    async def joshi(self, ctx):
+        #1x joshi!
+        try:
+            path ='/home/dash/data/.joshi'
+            files = os.listdir(path)
+            index = random.randrange(0, len(files))
+#            file = path+"/"+files[index]
+            fp = path
+            channel.send(file=discord.File(fp, files[index]))
+            
+        except:
+            await ctx.send("Nope")
+
+    def cog_unload(self):
+        self.bot.loop.create_task(self.session.close())
+
+    __del__ = cog_unload
