399bc5a5f38d043ed54607e7b93c00f18d20e6b5
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2020-06-06 08:44:15 +0930

    Add files via upload

--- penis/penis.py
+++ penis/penis.py
@@ -8,34 +8,25 @@ class Penis(commands.Cog):
     """Penis related commands."""
 
     @commands.command()
-    async def penis(self, ctx, *users: discord.Member):
-        """Detects user's penis length
+    async def penis(self, ctx: commands.Context, user: discord.Member = None) -> None:
+        """
+            pp
+        """
+        msg = " "
+        length = random.randint(0, 30)
+        msg = = "8{}D".format("=" * length)
+        if user:
+            if user.id == self.bot.user.id:
+                user = ctx.message.author
+                bot_msg: List[str] = [
+                    _("Bigger than yours!"),
+                    _("If i told you i'd have to fuck you!"),
+                    _("haha sorry motherfucker!"),
+                    _("You wish!"),
+                ]
+                await ctx.send(f"{ctx.author.mention} {choice(bot_msg)}")
 
-        This is 100% accurate.
-        Enter multiple users for an accurate comparison!"""
-        if not users:
-            await ctx.send_help()
-            return
-
-        dongs = {}
-        msg = ""
-        state = random.getstate()
-
-        for user in users:
-            random.seed(str(user.id))
-            length = random.randrange(1,30)
-            #if ctx.bot.user.id == user.id:
-            #    length = 50
-            #else:
-            #    length = random.randint(0, 30)
-
-            dongs[user] = "8{}D".format("=" * length)
-
-#        random.setstate(state)
-        dongs = sorted(dongs.items(), key=lambda x: x[1])
-
-        for user, dong in dongs:
-            msg += "**{}'s size:**\n{}\n".format(user.display_name, dong)
-
-        for page in pagify(msg):
-            await ctx.send(page)
+            else:
+                await ctx.send(user.mention + msg)
+        else:
+            await ctx.send(ctx.message.author.name+"**'s size is:**\n" + msg)
