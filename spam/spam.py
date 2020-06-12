1ee247c7e4c13cb43812d544918da0b47a9f6e34
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2020-06-12 16:56:32 +0930

    Add files via upload

--- spam/spam.py
+++ spam/spam.py
@@ -52,7 +52,8 @@ class Spam(BaseCog):
                 path = choice(joshi_path)
                 files = os.listdir(path)
                 index = random.randrange(1, len(files))
-                ctx.send("<@357228673331494913>")
+                try:
+                    await ctx.send("<@357228673331494913>")
                 await ctx.send(file=discord.File(path+"/"+files[index]))
             return     
         
