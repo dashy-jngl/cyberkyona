493a3dc41f47e97c8bb1dcde8e0c31136a40255c
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2020-07-03 06:15:14 +0930

    Add files via upload

--- penis/penis.py
+++ penis/penis.py
@@ -72,17 +72,17 @@ class Penis(commands.Cog):
                 user = ctx.message.author
                 simprate = random.randint(100, 200)
                 simp = str(simprate)
-                await ctx.send("**" + ctx.author.mention + "'s** simprate is:\n\n" + "**" +  simp +"%**")            
+                await ctx.send("**" + ctx.message.mention + "'s** simprate is:\n\n" + "**" +  simp +"%**")            
             elif user.id == 302168981903769602: #CryoXz
                 user = ctx.message.author
                 simprate = random.randint(100, 200)
                 simp = str(simprate)
                 await ctx.send("**" + ctx.author.mention + "'s** simprate is:\n\n" + "**" +  simp +"%**") 
             elif user.id == 574149508989190154: #tiger
-                user = ctx.message.author
+                #user = ctx.message.author
                 simprate = random.randint(100, 200)
                 simp = str(simprate)
-                await ctx.send("**" + ctx.author.mention + "'s** simprate is:\n\n" + "**" +  simp +"%**")    
+                await ctx.send("**" + ctx.user.nickname + "'s** simprate is:\n\n" + "**" +  simp +"%**")    
             else:
                 await ctx.send("**" + ctx.author.mention + "'s** simprate is:\n\n" + "**" +  simp +"%**")
         else:
