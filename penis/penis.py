e5eb3f5419b1a80f607cd9c312971a57a2999aaa
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2020-07-03 06:19:01 +0930

    Add files via upload

--- penis/penis.py
+++ penis/penis.py
@@ -69,15 +69,15 @@ class Penis(commands.Cog):
                 ]
                 await ctx.send(f"{choice(bot_msg)}")
             elif user.id == 338135974158794752: #dashy
-                user = ctx.message.author
+                #user = ctx.message.author
                 simprate = random.randint(100, 200)
                 simp = str(simprate)
-                await ctx.send("**" + ctx.message.mention + "'s** simprate is:\n\n" + "**" +  simp +"%**")            
+                await ctx.send("**" + user.name + "'s** simprate is:\n\n" + "**" +  simp +"%**")            
             elif user.id == 302168981903769602: #CryoXz
-                user = ctx.message.author
+                #user = ctx.message.author
                 simprate = random.randint(100, 200)
                 simp = str(simprate)
-                await ctx.send("**" + ctx.author.mention + "'s** simprate is:\n\n" + "**" +  simp +"%**") 
+                await ctx.send("**" + user.name + "'s** simprate is:\n\n" + "**" +  simp +"%**") 
             elif user.id == 574149508989190154: #tiger
                 #user = ctx.message.author
                 simprate = random.randint(100, 200)
