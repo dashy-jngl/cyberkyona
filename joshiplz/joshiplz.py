a0af18d8f0d6ee8adad2e50786b9d40f06c79894
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2020-08-16 14:48:19 +0930

    Add files via upload

--- joshiplz/joshiplz.py
+++ joshiplz/joshiplz.py
@@ -76,7 +76,7 @@ class Joshiplz(BaseCog):
         path = choice(joshi_path)
         try:
             for x in range(0,amount):
-                if user == 338135974158794752: #dashy
+                if user.id == 338135974158794752: #dashy
                     path == dashy_path
                 else:
                     path = choice(joshi_path)
