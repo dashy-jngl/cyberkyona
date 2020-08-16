43d0c5d91f442ccc6a68dabf1a1e7cc5a98eceb8
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2020-08-16 21:56:25 +0930

    Add files via upload

--- joshiplz/joshiplz.py
+++ joshiplz/joshiplz.py
@@ -77,14 +77,11 @@ class Joshiplz(BaseCog):
             for x in range(0,amount):
                 if user.id == 338135974158794752: #dashy
                     path = dashy_path
-                    files = os.listdir(path)
-                    index = random.randrange(1, len(files))
-                    await ctx.send(file=discord.File(path+"/"+files[index]))
                 else:
                     path = choice(joshi_path)
-                    files = os.listdir(path)
-                    index = random.randrange(1, len(files))
-                    await ctx.send(file=discord.File(path+"/"+files[index]))
+                files = os.listdir(path)
+                index = random.randrange(1, len(files))
+                await ctx.send(file=discord.File(path+"/"+files[index]))
             return     
         
         except:
