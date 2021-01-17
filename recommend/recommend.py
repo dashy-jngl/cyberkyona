4311ddf3e87eed003cc96ba2d3a433cd877a5fc3
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2021-01-18 07:31:20 +1030

    Add files via upload

--- recommend/recommend.py
+++ recommend/recommend.py
@@ -50,14 +50,19 @@ stardom: List[str] = [
     ("**Kairi Hojo vs Santana Garrett - Stardom 2016.05.15** https://m.bilibili.com/video/BV17x411G7tX"),
     ("**Kairi Hojo vs. Meiko Satomura - Stardom** https://m.bilibili.com/video/BV14W411j7Zw"),
     ("**Kairi Hojo vs. Toni Storm - Stardom ** https://m.bilibili.com/video/BV1mW411E71q"),
+    ("**DASH Chisako & Sendai Sachiko Vs  Io Shirai & Mayu Iwatani** https://www.youtube.com/watch?v=1s83jVycaRU"),
     #("**** "),
-    
+    #("**** "),
 ]
 sendai: List[str] = [
     ("**Dash Chisako vs. Sareee - 2019.07.07 Sendai Girls** https://m.bilibili.com/video/BV1Z4411C7Rs"),
     ("**Meiko Satomura vs.Toni Storm - 2019.07.27 Sendai Girls, Sendai UK** https://m.bilibili.com/video/BV14J41117po"),
     ("**Meiko Satomura vs.Sareee - 2019.04.16 Sendai Girls** https://m.bilibili.com/video/BV1Y4411A7iu"),
-    ("**Tag: DASH Chisako and Meiko Satomura and Syuri vs. Sakura Hirota and Chihiro Hashimoto and Yuu** https://www.youtube.com/watch?v=EdxRCoIv5EE")
+    ("**Tag: DASH Chisako and Meiko Satomura and Syuri vs. Sakura Hirota and Chihiro Hashimoto and Yuu** https://www.youtube.com/watch?v=EdxRCoIv5EE"),
+    ("**DASH Chisako vs Chihiro Hashimoto - Sendai Girls 2018.05.13** https://youtu.be/d2qpnM2x3RE"),
+    ("**Kagetsu vs Chihiro Hashimoto - Sendai Girls February 16th 2020** https://www.dailymotion.com/video/k7cBgl4mNB5auivUiyC"),
+    #("**** "),
+    #("**** "),
 ]
 agz: List[str] = [
     ("**Miyuki Takase vs Himeka Arita - AGZ 2019.11.06** https://m.bilibili.com/video/BV1s54y1s72x"),
@@ -70,6 +75,8 @@ agz: List[str] = [
     ("**Tequila Saya vs Hikari Shimizu - AGZ 2019.05.25** https://www.bilibili.com/video/BV19r4y1T7Kt/"),
     ("**Kakeru Sekiguchi & Miku Aono vs Mii & Rina Amikura - AGZ 2020.11.24** https://www.bilibili.com/video/BV16y4y1m75B/"),
     ("**Miyuki Takase & Himeka Arita vs Mari & SAKI - AGZ 2019.05.25** https://www.bilibili.com/video/BV1tU4y147qc/"),
+    #("**** "),
+    #("**** "),
 ]
 ice: List[str] = [
     ("**Tsukasa Fujimoto vs. Maya Yukihi - 2019.08.03 Ice Ribbon New Ice Ribbon #974 ~ Osaka Ribbon 2019 III** https://m.bilibili.com/video/BV1x7411k73E"),
@@ -81,6 +88,10 @@ ice: List[str] = [
 ]
 indy: List[str] = [
     ("**Kagetsu vs. Meiko Satomura - 2020.02.24 Kagestsu Produce** https://m.bilibili.com/video/BV19E411u79u"),
+    ("**Sareee & Yoshiko vs Zap I & Zap T from RJPW 2020** https://youtu.be/EkoNlVXwopE"),
+    ("**Nixon Newell vs. Kimber Lee - WCPW True Legacy #7** https://youtu.be/mvBZ5zY1D_U"),
+    ("**Sadie Gibbs [AEW] vs Kay Lee Ray [WWE NXTUK] - EVE** https://www.youtube.com/watch?v=oGIKgAS2aQ4&list=LL&index=2"),
+    #("**** "),
 ]
 wwe: List[str] = [
     ("**Charlotte Flair Vs IO Shirai Vs Rhea Ripley - NXT** https://www.dailymotion.com/video/x7ucx0n"),
@@ -90,31 +101,64 @@ wwe: List[str] = [
     ("**Steel Cage: Rhea Ripley vs. Mercedes Martines - NXT** https://www.dailymotion.com/video/x7w20gy"),
     ("**Asuka vs. Bayley - NXT** https://www.dailymotion.com/video/x656jeb"),
     ("**Ember Moon vs. Asuka - NXT** https://www.dailymotion.com/video/x5xiing"),
+    #("**** "),
+    #("**** "),
 ]
 tjp: List[str] = [
     ("**Yuka Sakazaki vs. Mizuki - 2020.11.07 TJPW Wrestle Princess** https://m.bilibili.com/video/BV1wy4y1z76y"),
+    ("**Miyu Yamashita vs. Allysin Kay - SHINE Championship - TJP** https://youtu.be/jRAPC3mNrME"),
     #("**** "),
 ]
 wave: List[str] = [
     ("**Kana vs Kana (Sakura Hirota) - Wave 2015** https://www.youtube.com/watch?v=oBc04ILjeCM"),
     ("**WAVE tournament final: Takumi vs Nagisa Nozaki vs Ryo Mizunami - Wave CTW 2019-07-15** https://youtu.be/R41Q6glbd8w"),
+    ("**Takumi vs HIRO'e for the Regina di wave title. - Wave 2019.08.12** https://youtu.be/ljvY8JiP9qg"),
+    ("**Luminous (Miyuki Takase & Haruka Umesaki) vs Yuu & Ami Miura - Wave 2020.11.1** https://youtu.be/Mxd3CZ1gcU4"),
+    ("**HIRO'e vs Arisa Nakajima - Wave** https://youtu.be/w3Swv_O-giU"),
+    ("**HIRO'e vs Tsukasa - Wave** https://youtu.be/XKExTXEA2_o"),
+    ("**Sareee & Hibiki vs Rina Amikura & Rina Shingaki from WAVE** https://youtu.be/a5L4E4Flum4"),
+    ("**Syuri vs Rin Kadokura from WAVE ** https://youtu.be/arzbiulGYcU"),
+    ("**Tsukasa Fujimoto vs Haruka Umesaki - Wave** https://youtu.be/iBUH1D4tzHw"),
+    ("**Takumi Iroha & Yuki Miyazaki vs Mio Momono & Yumi Ohka from September 6th 2020.** https://youtu.be/TOfwcZoeNnU"),
+    ("**Takumi Iroha vs Yumi Ohka for the Regina di WAVE title from November 23rd 2019. Really good match.** https://youtu.be/Ox8PTsc1XeI"),
+    ("**HIRO'e & Miyuki Takase vs Yuki Miyazaki & Nagisa Nozaki for the WAVE tag titles from June 28th 2018.** https://youtu.be/8ijKXOTSKJI"),
 ]
 marv: List[str] = [
     ("**Syuri & Takumi vs Nyla Rose and ASUKA - Marvelous 2019.12.08** https://youtu.be/KF2eKFT6NjE"),
     ("**Mio vs Takumi- Marvelous 2019.11.03** https://youtu.be/ovQxvCKJeao"),
-    #("**** "),
-    #("**** "),
-    #("**** "),
+    ("**Takumi Iroha & Syuri vs Nagisa Nozaki & Chihiro Hashimoto - Marvelous** "),
     #("**** "),
 ]
 sead: List[str] = [
     ("**Arisa Nakajima (c) vs. Rina Yamashita, SEAdLINNNG New Leaf! (10.03.2020)** https://youtu.be/qjGE9NEMAIc"),
+    ("**Arisa Nakajima & Tsukasa Fujimoto vs Sareee & Nanae Takahashi. - Sead May 5th 2017** https://www.dailymotion.com/video/k1ybFIWXlxMGqGndRlP"),
+    ("**Arisa Nakajima & Nanae Takahashi vs. Hiroyo Matsumoto & Ryo Mizunami** https://www.dailymotion.com/video/x5ggl8s"),
+    #("**** "),
+    #("**** "),
 ]
 oz: List[str] = [
     ("**Hiroyo Matsumoto vs. Hikari Shida - OZ Academy Plum Hanasaku 2017.08.20** https://m.bilibili.com/video/BV1f441177F8"),
     ("**Mayumi Ozaki and Maya Yuhiki vs Rina Yamashita and Yoshiko - OZ Academy Flower Bloom In Yokohama 2018** https://m.bilibili.com/video/BV1Nt411Z7HB"),
+    ("**Mayumi Ozaki vs  Saori Anou - Oz Acadmey** https://youtu.be/zNUTR9PEbsM"),
+    ("**Hikaru Shida & Syuri vs. Hiroyo Matsumoto & Kagetsu 2017.01.25** https://www.dailymotion.com/video/x7qx2iw"),
+    #("**** "),
+    #("**** "),
+    #("**** "),
 ]
-
+retro: List[str] = [
+    ("**Esther Moreno y Manami Toyota vs Aja Kong y Bison Kimura. 29-Abril-1991** https://youtu.be/z_pLjPK3ygI"),
+    ("**Kana VS Meiko Satomura - 2-13-2011 (Triple Tails Osaka event)** https://www.youtube.com/watch?v=Qx1B35rVcnw"),
+    ("**Debbie Malenko & Sakie Hasegawa vs Takako Inoue & Mariko Yoshida from Wrestlemarinepiad 1992** https://www.dailymotion.com/video/xwa9w0"),
+    ("**Dynamite Kansai vs Manami Toyota WWWA Title match from December 4th 1995.** https://youtu.be/XypDvsj6t1Y"),
+    ("**Aja Kong & Sakie Hasegawa vs Manami Toyota & Toshiyo Yamada. WWWA Tag Title 2/3 falls match from February 27th 1994.** https://youtu.be/Pov70LNERJM"),
+    ("**Bull Nakano & Aja Kong vs Akira Hokuto & Shinobu Kandori. Hokuto & Kandori are bitter rivals and are forced to team together to face Nakano & Kong. An absolute classic, highly recommend.(BU113TPR00FJAYCE#2404)** https://youtu.be/cTMaGmB45xA"),
+    ("**Manami Toyota vs. Aja Kong - March 26, 1995** https://youtu.be/OrE-o-L3RZ4"),
+    ("**Combat Toyoda vs. Megumi Kudo (No-Rope Electrified Barbwire Deathmatch)** https://www.youtube.com/watch?v=BSdMiVV_fnQ"),
+    ("**Dynamite Kansai, AKINO & GAMI vs Amazing Kong, Mayumi Ozaki & Ayako Hamada - Gaia** https://youtu.be/t4b57zLQpPU"),
+    ("**Kana vs. Meiko Satomura (4/19/2010)** https://www.youtube.com/watch?v=Hhv86X56XGM&ab_channel=vrnamnchnm"),
+    #("**** "),
+    #("**** "),
+]
 termlist = [
     (stardom),
     (agz),
@@ -127,6 +171,7 @@ termlist = [
     (wave),
     (sead),
     (oz),
+    (retro),
 ]
 class Recommend(commands.Cog):
 
