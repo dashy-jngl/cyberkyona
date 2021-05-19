385ea040364419e9a6daac0731290cbfea2dd89c
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2021-05-19 21:42:37 +0930

    Add files via upload

--- joshiplz/joshiplz.py
+++ joshiplz/joshiplz.py
@@ -67,7 +67,7 @@ class Joshiplz(BaseCog):
 
     @commands.command()
 #    @commands.cooldown(1, 60, commands.BucketType.guild)
-    async def joshiplz(self, ctx: commands.Context, amount : int = 3, joshi: Optional[str]):
+    async def joshiplz(self, ctx: commands.Context, joshi: Optional[str], amount : int = 3):
         """ - Throws a Joshi bomb!
         Defaults to 3, max is 500"""
         if not amount:
