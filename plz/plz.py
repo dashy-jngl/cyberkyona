c1731b440a07b1a8b0bd987a8c885c0446a08c89
dashy-jngl <66165158+dashy-jngl@users.noreply.github.com>
2024-04-08 15:21:08 +0930

    Update plz.py

--- plz/plz.py
+++ plz/plz.py
@@ -203,9 +203,9 @@ class Plz(BaseCog):
         if ask == "ksup":
             ask = choice(ksup)
         
-        user = ctx.message.author
-        if user.id == 151823155340509186 or user.id == 734768348201615400:
-            return
+        # user = ctx.message.author
+        # if user.id == 151823155340509186 or user.id == 734768348201615400:
+        #     return
         try:
             try:
                 path = base_path + ask
