67ef98424185f1963de94687b68f8d8073d56241
dash <hash.chisako@gmail.com>
2024-08-13 10:49:43 +0930

    keyona

--- stardom/stardom.py
+++ stardom/stardom.py
@@ -9,37 +9,6 @@ class StardomCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # def scrape_stardom_schedule(self):
-    #     """Scrape the Stardom schedule page and extract match details."""
-    #     schedule_url = "https://wwr-stardom.com/schedule/"
-
-    #     # Get the page content
-    #     response = requests.get(schedule_url)
-    #     soup = BeautifulSoup(response.text, 'html.parser')
-
-    #     # Find the first show link
-    #     first_show_element = soup.find('a', {'class': 'mc-link'})
-
-    #     if first_show_element is None:
-    #         # Log the error or inform the user that the element wasn't found
-    #         return []
-
-    #     first_show_link = first_show_element.get('href')
-
-    #     # Now, get the content of the first show
-    #     show_response = requests.get(first_show_link)
-    #     show_soup = BeautifulSoup(show_response.text, 'html.parser')
-
-    #     # Example: Extract the first match (You can loop over all matches)
-    #     match_info = []
-    #     matches = show_soup.find_all('p')  # Assuming each match is inside a <p> tag
-
-    #     for match in matches:
-    #         if '◆' in match.text:  # Simple filter to get match details
-    #             match_info.append(match.text.strip())
-
-    #     return match_info
-
     def scrape_stardom_schedule(self):
         """Scrape the Stardom schedule page and extract match details."""
         schedule_url = "https://wwr-stardom.com/schedule/"
@@ -48,18 +17,18 @@ class StardomCog(commands.Cog):
         response = requests.get(schedule_url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Try to find all show links - this approach assumes all shows are within a specific tag
-        # Example: This assumes that each show link is within a div with a specific class or similar
-        show_links = soup.find_all('a', href=True)
+        # Find the first show link within the <ul> with id 'upcoming-events-c6674bbda7f981637828f635a37cbeaa'
+        upcoming_events = soup.find('ul', {'id': 'upcoming-events-c6674bbda7f981637828f635a37cbeaa'})
 
-        # Filter links that point to show pages (You may need to adjust the condition)
-        show_links = [link['href'] for link in show_links if '/schedule/' in link['href']]
-
-        if not show_links:
-            # Log the error or inform the user that the element wasn't found
+        if not upcoming_events:
             return []
 
-        first_show_link = show_links[0]
+        first_show_element = upcoming_events.find('a', href=True)
+        
+        if not first_show_element:
+            return []
+
+        first_show_link = first_show_element['href']
 
         # Now, get the content of the first show
         show_response = requests.get(first_show_link)
@@ -74,7 +43,6 @@ class StardomCog(commands.Cog):
                 match_info.append(match.text.strip())
 
         return match_info
-
 
     @commands.command()
     async def stardom(self, ctx):
