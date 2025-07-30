279bf37529c03f3bb861b44e981395e76ee1440c
Dashy9000 <66165158+dashy-jngl@users.noreply.github.com>
2025-07-30 16:52:58 +0930

    Update plz.py

--- plz/plz.py
+++ plz/plz.py
@@ -21,8 +21,8 @@ choices_map = {
     "samus": ["tam nakano", "saya iida"],
     "samusbots": ["tam nakano", "saya iida"],
     "inky": ["hina", "starlight"],
-    "bby": ["azm", "hazuki"],
-    "bobby": ["azm", "hazuki"],
+    "bby": ["azm", "hazuki", "rika"],
+    "bobby": ["azm", "hazuki", "rika"],
 }
 
 substitutions = {
